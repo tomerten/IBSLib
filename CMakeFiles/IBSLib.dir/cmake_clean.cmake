f595248a17842c439a2679163982ef575571ecce
Tom Mertens <your.email@whatev.er>
2021-06-01 16:21:14 +0200

    added cmake cache to gitignore

---
+++ CMakeFiles/IBSLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IBSLib.dir/libtest.cpp.o"
+  "IBSLib.pdb"
+  "IBSLib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IBSLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/numfunc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numfunc.dir/NumericFunctions/NumericFunctions.cpp.o"
+  "libnumfunc.pdb"
+  "libnumfunc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numfunc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
